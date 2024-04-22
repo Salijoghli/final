c3394ff7a5516348ff247744e049f8d9b12d3729
Guga Kakhadze <107577102+Salijoghli@users.noreply.github.com>
2024-04-22 12:18:15 +0400

    Add files via upload

---
+++ face-detector.js
@@ -0,0 +1,71 @@
+const app = document.querySelector("#app");
+const button = document.createElement("button");
+button.textContent = "Toggle Landmarks";
+let drawLandmarks = true;
+
+button.addEventListener("click", () => {
+  drawLandmarks = !drawLandmarks;
+});
+
+const video = document.createElement("video");
+video.setAttribute("autoplay", "");
+video.setAttribute("muted", "");
+video.width = 720;
+video.height = 560;
+
+app.appendChild(button);
+app.appendChild(video);
+
+const startVideo = async () => {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    video.srcObject = stream;
+  } catch (err) {
+    console.error("Error accessing camera:", err);
+  }
+};
+
+const loadModels = async () => {
+  try {
+    await Promise.all([
+      faceapi.nets.tinyFaceDetector.loadFromUri("./models"),
+      faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
+      faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
+      faceapi.nets.faceExpressionNet.loadFromUri("./models"),
+    ]);
+    console.log("Recording");
+    startVideo();
+  } catch (err) {
+    console.error("Failed to load models:", err);
+  }
+};
+
+const faceDetectionLoop = async (canvas, displaySize) => {
+  const detections = await faceapi
+    .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+    .withFaceLandmarks()
+    .withFaceExpressions();
+  const resizedDetections = faceapi.resizeResults(detections, displaySize);
+  canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+  faceapi.draw.drawDetections(canvas, resizedDetections);
+  drawLandmarks && faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+  faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+};
+
+const handleFaceDetection = () => {
+  const canvas = faceapi.createCanvasFromMedia(video, {
+    willReadFrequently: true,
+  });
+  document.body.append(canvas);
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+  const animate = () => {
+    requestAnimationFrame(animate);
+    faceDetectionLoop(canvas, displaySize);
+  };
+  animate();
+};
+
+loadModels();
+
+video.addEventListener("play", handleFaceDetection);
