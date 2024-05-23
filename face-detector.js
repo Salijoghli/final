5282488a4013ee8b5aec6adedd8e9b977a2b5f6c
Salijoghli <guga.kakhadze12@gmail.com>
2024-05-23 15:34:03 +0400

    made changes in the detection. added recognition page and working on it

--- face-detector.js
+++ face-detector.js
@@ -1,25 +1,56 @@
-const app = document.querySelector("#app");
-const button = document.createElement("button");
-button.textContent = "Toggle Landmarks";
-let drawLandmarks = false;
+const buttonTogglePause = document.getElementById("togglePause");
+const video = document.getElementById("video");
+const app = document.getElementById("app");
+const loadingDiv = document.getElementById("loading");
+let isPaused = false;
+let loading = true;
+const state = {
+  drawLandmarks: false,
+  drawExpressions: false,
+  drawAgeGender: false,
+};
 
-button.addEventListener("click", () => {
-  drawLandmarks = !drawLandmarks;
+const buttons = [
+  { id: "toggleLandmarks", state: "drawLandmarks" },
+  { id: "toggleExpressions", state: "drawExpressions" },
+  { id: "toggleAgeGender", state: "drawAgeGender" },
+];
+
+const toggleButtons = (state) => {
+  buttons.forEach((button) => {
+    document.getElementById(button.id).disabled = state;
+  });
+};
+
+buttons.forEach((button) => {
+  const element = document.getElementById(button.id);
+  element.addEventListener("click", () => {
+    state[button.state] = !state[button.state];
+  });
 });
 
-const video = document.createElement("video");
-video.setAttribute("autoplay", "");
-video.setAttribute("muted", "");
-video.width = 720;
-video.height = 560;
-
-app.appendChild(button);
-app.appendChild(video);
+buttonTogglePause.addEventListener("click", () => {
+  isPaused = !isPaused;
+  if (isPaused) {
+    video.pause();
+    buttonTogglePause.textContent = "Resume";
+    toggleButtons(true);
+  } else {
+    video.play();
+    buttonTogglePause.textContent = "Pause";
+    toggleButtons(false);
+  }
+});
 
 const startVideo = async () => {
   try {
-    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: false,
+    });
     video.srcObject = stream;
+    app.style.display = "block";
+    loading = false;
   } catch (err) {
     console.error("Error accessing camera:", err);
   }
@@ -27,79 +58,59 @@ const startVideo = async () => {
 
 const loadModels = async () => {
   try {
+    loadingDiv.style.display = "block";
     await Promise.all([
       faceapi.nets.tinyFaceDetector.loadFromUri("./models"),
       faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
-      faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
-      faceapi.nets.faceExpressionNet.loadFromUri("./models"),
       faceapi.nets.ageGenderNet.loadFromUri("./models"),
-      faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
+      faceapi.nets.faceExpressionNet.loadFromUri("./models"),
     ]);
     console.log("Recording");
     startVideo();
   } catch (err) {
     console.error("Failed to load models:", err);
+  } finally {
+    loadingDiv.style.display = "none";
   }
 };
 
-const handleFaceRecognition = async (detection, canvas) => {
-  const labeledFaceDescriptors = await loadLabeledImages();
-  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
-  const bestMatch = faceMatcher.findBestMatch(detection.descriptor);
-
-  const box = detection.detection.box;
-  const drawBox = new faceapi.draw.DrawBox(box, {
-    label: bestMatch.toString(),
-  });
-  drawBox.draw(canvas);
-};
-
 const faceDetectionLoop = async (canvas, displaySize) => {
+  if (isPaused) return;
   const detections = await faceapi
-    .detectAllFaces(video)
+    .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
     .withFaceLandmarks()
     .withFaceExpressions()
-    .withFaceDescriptors()
     .withAgeAndGender();
   const resizedDetections = faceapi.resizeResults(detections, displaySize);
   canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
   faceapi.draw.drawDetections(canvas, resizedDetections);
-  drawLandmarks && faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-  faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
-  resizedDetections.forEach((detection) => {
-    handleFaceRecognition(detection, canvas);
-  });
-};
-
-const loadLabeledImages = async () => {
-  const labels = ["guga"];
-  const labeledFaceDescriptors = [];
-
-  for (const label of labels) {
-    const descriptions = [];
-    for (let i = 1; i <= 4; i++) {
-      const img = await faceapi.fetchImage(
-        `https://raw.githubusercontent.com/Salijoghli/final/main/images//${label}/${label} (${i}).jpg`
-      );
-      const detections = await faceapi
-        .detectSingleFace(img)
-        .withFaceLandmarks()
-        .withFaceDescriptor();
-      descriptions.push(detections.descriptor);
-    }
-    labeledFaceDescriptors.push(
-      new faceapi.LabeledFaceDescriptors(label, descriptions)
-    );
-  }
-
-  return labeledFaceDescriptors;
+  state.drawLandmarks &&
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+  state.drawExpressions &&
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+  state.drawAgeGender &&
+    resizedDetections.forEach((detection) => {
+      const box = detection.detection.box;
+      let textVerticalPosition = box.y + box.height - 10;
+      const ageGenderText =
+        Math.round(detection.age) + " year old " + detection.gender;
+      canvas.getContext("2d").font = "16px Arial";
+      canvas.getContext("2d").fillStyle = "white";
+      canvas
+        .getContext("2d")
+        .fillText(ageGenderText, box.x + 3, textVerticalPosition);
+    });
 };
 
 const handleFaceDetection = () => {
   const canvas = faceapi.createCanvasFromMedia(video, {
     willReadFrequently: true,
   });
-  document.body.append(canvas);
+  document.getElementById("app").insertAdjacentElement("afterend", canvas);
+  const appRect = document.getElementById("app").getBoundingClientRect();
+  const canvasTop = appRect.bottom + 10;
+  canvas.style.position = "absolute";
+  canvas.style.top = canvasTop + "px";
   const displaySize = { width: video.width, height: video.height };
   faceapi.matchDimensions(canvas, displaySize);
   const animate = () => {
@@ -109,6 +120,6 @@ const handleFaceDetection = () => {
   animate();
 };
 
-await loadModels();
+loadModels();
 
 video.addEventListener("play", handleFaceDetection);
